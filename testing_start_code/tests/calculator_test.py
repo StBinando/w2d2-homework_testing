abe36efadd22ace0f5264286cd286c6ce5be9fcf
Stefano Binando <sbinando@gmail.com>
2021-10-26 18:19:08 +0100

    test_subtract__returns_negative_4

--- testing_start_code/tests/calculator_test.py
+++ testing_start_code/tests/calculator_test.py
@@ -4,3 +4,7 @@ from src.calculator import add, divide, multiply, subtract
 class TestCalculator(unittest.TestCase):
     def test_add__returns_10(self):
         self.assertEqual(10, add(3, 7))
+
+    def test_subtract__returns_negative_4(self):
+        self.assertEqual(-4, subtract(3, 7))
+        
